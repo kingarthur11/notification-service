ecacc071b16ffe0daa2b408ed4ed26f1a4dc3bff
kingarthur11 <orduharthur@gmail.com>
2024-03-12 20:20:28 +0100

    first commit

---
+++ src/app/Jobs/CreateUserJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class CreateUserJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public $user;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::info('User detail save successfully as', [
+            'id' => $this->user->id,
+            'firstName' => $this->user->firstName,
+            'lastName' => $this->user->lastName,
+            'email' => $this->user->email
+        ]);
+    }
+}
